f228b6468a80fa293e10e160efd3a43a11277de8
OverIQ429 <67504104+OverIQ429@users.noreply.github.com>
2026-08-08 17:10:53 +0300

    Update security.py

--- security.py
+++ security.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from fastapi import HTTPException, status
 
-SECRET_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhQdgJeLP5dUnRRM62+YR61Zum1HftdnMLszXvQ7or2s79qtgr+9ewOQGRxwCkDV499LbnKu9nC1+RQaGMnEm0sgyYavOVTh6xEF/niPofa7QohntloLFyfLvP9rnrRKkl9eJFUpWliFq2FTCwjdwb783/s9FIm9FRPzuiMza+i2l5qhwPyK9DXruki+6tQk3hL7lA8r+RJ2Gc+np26c+DCN489DuQfyLrlXGGyudRf8myJXmD67qklXeTH+Mh3jq9ufdTWhBKvu4RooxhG37iQuSiRzFxccY3Exj8fbaz+DJzuS8owHp8jsXUC3tFQF0lDNAEggV2oApo7wT+grbVQIDAQAB"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
